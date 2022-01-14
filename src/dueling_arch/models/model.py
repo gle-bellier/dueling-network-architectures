2492c91c6284212cf1fd084a1a3ebde0ad78cd38
Georges Le Bellier <glebellier@protonmail.com>
2022-01-14 14:17:21 +0100

    Add first model with dueling architecture

--- src/dueling_arch/models/model.py
+++ src/dueling_arch/models/model.py
@@ -3,8 +3,26 @@ import torch.nn as nn
 
 
 class Network(nn.Module):
-    def __init__(self):
+    """Neural netword with a dueling architecture"""
+    def __init__(self, in_size, h_size, n_actions):
         super(Network, self).__init__()
 
+        self.main = nn.Sequential(nn.Linear(in_size, h_size * 2), nn.ReLU(),
+                                  nn.Linear(h_size * 2, h_size), nn.ReLU())
+
+        # block in charge of state value estimation
+        self.state_block = nn.Sequential(nn.Linear(h_size, 1))
+
+        # block in charge of advantage value estimation
+        self.adv_block = nn.Sequential(nn.Linear(h_size, n_actions))
+
     def forward(self, x):
-        return x**2
+        x = self.main(x)
+
+        v = self.state_block(x)
+        a = self.adv_block(x)
+
+        # compute q values:
+
+        q = v + a - a.mean()
+        return q
